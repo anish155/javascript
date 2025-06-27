957b6029a22160213572052ab55d340426f88097
anish155 <anishtandukar3@gmail.com>
2025-06-27 17:39:03 +0545

    Bulb

---
+++ 2025-27-06/print.js
@@ -0,0 +1,34 @@
+// console.log("2"+2+"2")
+
+// console.log("hi"+2)
+// console.log(2+2)
+// alert("welcome to basics")
+
+/*let name="anish";
+const age=20;
+console.log(name);
+console.log(age)*/
+
+/*let person={
+    name:"anish",
+    age:20
+};
+
+
+
+console.log(person.age)
+console.log(person["name"])*/
+
+/*let names=["anish","kumar","raj"]
+names[3]="joshi"
+console.log(names)
+console.log(names[0])
+console.log(names.length)*/
+
+function bulbOn(){
+    document.getElementById("bulb").src="pic_bulbon.gif";
+}
+
+function bulbOff(){
+    document.getElementById("bulb").src="pic_bulboff.gif";
+}
