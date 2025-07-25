b997b79ca21125905d699181a4592142d244ad66
anish155 <anishtandukar3@gmail.com>
2025-07-25 17:23:59 +0545

    to fing missing number

---
+++ 2025-25-07/missing/missing.js
@@ -0,0 +1,20 @@
+function addInput() {
+    const inputDiv = document.getElementById("inputs");
+    const input = document.createElement("input");
+    input.type = "number";
+    input.className = "num";
+    inputDiv.appendChild(input);
+    inputDiv.appendChild(document.createElement("br"));
+}
+
+function findMissing() {
+    const inputs = document.getElementsByClassName("num");
+    const numbers = Array.from(inputs).map(i => parseInt(i.value));
+
+    const n = numbers.length + 1;
+    const expectedSum = (n * (n + 1)) / 2;
+    const actualSum = numbers.reduce((a, b) => a + b, 0);
+    const missing = expectedSum - actualSum;
+
+    alert("Missing number is: " + missing);
+}
